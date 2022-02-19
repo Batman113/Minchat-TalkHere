fb5bff08c9c8d2a49ced9284d2fabd28df556d85
Batman113 <86890562+Batman113@users.noreply.github.com>
2022-02-19 20:11:06 +0530

    Added ajax in post

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,30 +1,33 @@
 const Post = require('../models/posts');
 const UserData = require('../models/users');
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // return res.send('<h1>Express is up</h1>');
     //populating the user before sending thae data
-    Post.find({})
+try{
+    
+    let posts = await Post.find({})
+    .sort('-createdAt')
     .populate('user')
     .populate({
         path:'comments',
         populate:{
             path:'user'
         }
-    })
-    .exec(function(err,post){
-        if(err){
-            console.log(err);
-            return;
-        }
-        // console.log(post);
-        UserData.find({},function(err,User){
-            return res.render('home',{
-                title:"Minchat | Post Here",
-                Posts:post,
-                User:User
-            });
-        })
     });
+
+        // console.log(post);
+    let users = await UserData.find({});
+
+    return res.render('home',{
+        title:"Minchat | Post Here",
+        Posts:posts,
+        User:users
+    });
+}catch(err){
+    console.log(err);
+    return res.redirect('back');
+}
+
 }
 
 module.exports.profile = (req,res) => {
--- controller/postController.js
+++ controller/postController.js
@@ -1,34 +1,63 @@
 const Post = require('../models/posts');
 const Comment = require('../models/comment'); 
-module.exports.create = function (req,res){
-    if(req.user._id){
-        Post.create({
+module.exports.create = async function (req,res){
+    try{
+        let post = await Post.create({
         title:req.body.title,
         content:req.body.content,
         user:req.user.id
-    },function(err,post){
-        if(err){
-            console.log('Error | Creating post');
-            return;
+        });
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:"Post created"
+            })
         }
+
+
         return res.redirect('back');
-    });
-    }
-    else{
+    
+    }catch(error){
         return res.redirect('back');
     }
 }
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id,function(err,post){
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
         if(post.user == req.user.id){
             post.remove();
-            Comment.deleteMany({post:req.query.id},function(err){
-                return res.redirect('back');
-            })
-            
-        }else{
+
+            await Comment.deleteMany({post:req.query.id});
+           if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        postId:req.params.id
+                    },
+                    message:"post deleted"
+                })
+            }
             return res.redirect('back');
         }
-    });
+        else{
+            return res.redirect('back');
+        }
+    }catch(err){
+        return res.redirect('back');
+    }
+
+
+    // Post.findById(req.params.id,function(err,post){
+    //     if(post.user == req.user.id){
+    //         post.remove();
+    //         Comment.deleteMany({post:req.query.id},function(err){
+    //             return res.redirect('back');
+    //         })
+            
+    //     }else{
+    //         return res.redirect('back');
+    //     }
+    // });
 }
